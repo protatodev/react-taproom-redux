e8b7d84cc008f1c5eb1a103f159c101c869dc967
protatodev <thaddonaghue@yahoo.com>
2018-09-17 10:23:33 -0700

    wire up kegform and allow the creation and editing of kegs

--- src/components/KegForm.js
+++ src/components/KegForm.js
@@ -1,19 +1,90 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
-const KegForm = (props) => {
+class KegForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: props.keg ? props.keg.name : '',
+      price: props.keg ? props.keg.price : '',
+      brand: props.keg ? props.keg.brand : '',
+      description: props.keg ? props.description : '',
+      errorMsg: ''
+    };
+  }
+
+  onNameChange = e => {
+    const name = e.target.value;
+    this.setState(() => ({name}));
+    if(this.state.price) {
+      this.setState(() => ({  errorMsg: ''  }));
+    }
+  };
   
-  return(
-    <div>
-      <form>
-        <input type="text" placeholder="Name the new keg"/>
-        <input type="text" placeholder="Price the new keg"/>
-        <input type="text" placeholder="Enter a brand"/>
-        <textarea placeholder="Enter a description for the new keg"></textarea>
-        <button type="submit">Add Keg</button>
-      </form>
-    </div>
-  );
-};
+  onPriceChange = e => {
+    const price = e.target.value;
+    if(!price || price.match(/^\d{1,}(\.\d{0,2})?$/)) {
+      this.setState(() => ({ price }));
+    }
 
-export default connect()(KegForm);
+    if(this.state.name) {
+      this.setState(() => ({ errorMsg: '' }));
+    }
+  };
+
+  onBrandChange = e => {
+    const brand = e.target.value;
+    this.setState(() => ({ brand }))
+  }
+
+  onDescriptionChange = e => {
+    const description = e.target.value;
+    this.setState(() => ({ description }))
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    this.props.onSubmit({
+      name: this.state.name,
+      price: parseFloat(this.state.price),
+      brand: this.state.brand,
+      description: this.state.description
+    });
+  }
+  
+  render() {
+    return(
+      <div>
+        {this.state.errorMsg && <p>You must enter a <strong>NAME</strong> and a <strong>PRICE</strong> at the very least!</p>}
+        <form onSubmit={this.onSubmit}>
+          <input 
+            type="text" 
+            placeholder="Name the new keg"
+            value={this.state.name}
+            onChange={this.onNameChange}
+          />
+          <input 
+            type="text" 
+            placeholder="Price the new keg"
+            value={this.state.price}
+            onChange={this.onPriceChange}
+          />
+          <input 
+            type="text" 
+            placeholder="Enter a brand"
+            value={this.state.brand}
+            onChange={this.state.onBrandChange}
+          />
+          <textarea 
+            placeholder="Enter a description for the new keg"
+            value={this.state.description}
+            onChange={this.state.onDescriptionChange}  
+          ></textarea>
+          <button type="submit">Add Keg</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default KegForm;
